29fd009016c57bc8187618a14340bcfd948b5d4c
Paula <psanzilundain@gmail.com>
2024-05-24 12:30:57 +0200

    media queries tablet and desktop

--- web/src/components/MovieCard.jsx
+++ web/src/components/MovieCard.jsx
@@ -7,17 +7,29 @@ function MovieCard({ movies, randomNumber }) {
 
   return (
     <div className='card'>
-      <h2 className='title'>{movies[randomNumber].title}</h2>
-      <h2 className='director'>{movies[randomNumber].director}</h2>
-      <p className='year'>
-        {movies[randomNumber].year} {movies[randomNumber].country}
-      </p>
-      <img className='film_image' src={movies[randomNumber].poster} alt='' />
-      <h6 className='cast'>{movies[randomNumber].cast}</h6>
-      <h5 className='type'>{movies[randomNumber].type}</h5>
-      <p className='description_container'>
-        {movies[randomNumber].description}
-      </p>
+      <div className='container1'>
+        <h2 className='title'>{movies[randomNumber].title}</h2>
+        <h2 className='director'>{movies[randomNumber].director}</h2>
+        <p className='year'>
+          {movies[randomNumber].year} {movies[randomNumber].country}
+        </p>
+      </div>
+      <div className='try'>
+        <div className='container-image'>
+          <img
+            className='film_image'
+            src={movies[randomNumber].poster}
+            alt=''
+          />
+        </div>
+        <div className='container2'>
+          <h6 className='cast'>{movies[randomNumber].cast}</h6>
+          <h5 className='type'>{movies[randomNumber].type}</h5>
+          <p className='description_container'>
+            {movies[randomNumber].description}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
